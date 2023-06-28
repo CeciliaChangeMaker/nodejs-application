3657cec2ad832006b101ffab12bb7ce0a79e7b15
Cecilia Amabo <134659966+CeciliaChangeMaker@users.noreply.github.com>
2023-06-28 23:41:24 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.254.171.16:9000/',
+  serverUrl: 'http://54.246.42.73:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
